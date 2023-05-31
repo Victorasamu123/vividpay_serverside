1db1daf233a6b749eaa807543ce4200691d8ee44
Avanin <asamuvictor03@gmail.com>
2023-05-31 14:02:57 +0100

    auth_route_controller_model

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 require('dotenv').config()
+// middleware
 app.use(cors())
 app.use(express.urlencoded({limit: "200mb",extended:true}));
 app. use(express. json({limit: '200mb'}));
 const PORT  = process.env.PORT || 5000
 const URI  = process.env.MONGODB_URI
+//mongodb connection
 mongoose.set('strictQuery', false)
 
 const connection = async()=>{
   try {
     const connection_try = await mongoose.connect(URI)
-    console.log("connection to mongoose was sucessful");
-  } catch (error) {
+    console.log(`Mongoose connected to MongoDB`);
+} catch (error) {
     console.log(error)
-  }
+}
 }
 
 connection();
+
+//routes
+const auth = require('./routes/auth.route');
+app.use('/auth',auth)
+
+//Port area
 app.listen(PORT, ()=>{
     console.log(`app is runningat PORT: ${PORT}`)
-})
+});
---
+++ routes/auth.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const auth = express.Router();
+
+
+
+
+
+
+
+
+module.exports = auth;
